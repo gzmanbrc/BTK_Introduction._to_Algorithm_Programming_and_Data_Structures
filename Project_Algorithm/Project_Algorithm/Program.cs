35a8056bd11bc6e39cbe5c02c5f4912126e4cbcb
Gizem Anbarcı <gzmanbrc@outlook.com>
2025-01-25 22:33:59 +0300

    Started the course

---
+++ Project_Algorithm/Project_Algorithm/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+
+/* 25/01/2025 BTK-algoritma-programlama-ve-veri-yapilarina-giris  */
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var y; // if you dont know what type your variable you can use var 
+
+            Console.WriteLine(y.GetType); // use for know the type
+
+
+
+            /* */
+        }
+
+    }
+}
